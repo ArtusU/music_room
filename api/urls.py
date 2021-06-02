636eca8bb69038b136157d9cf96d00b742d9dfb5
ArtusU <artusrock@hotmail.com>
2021-06-02 09:54:00 +0100

    JoinRoom view

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom
 
 app_name = 'api'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('room', RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
     path('get-room', GetRoom.as_view()),
+    path('join-room', JoinRoom.as_view()),
 ]
