4c511b0cb71edab7df6b88c6a8843ee4be5267ec
ArtusU <artusrock@hotmail.com>
2021-06-06 11:11:51 +0100

    Room.js authenticateSpotify

--- spotify/info.credentials.py
+++ spotify/info.credentials.py
@@ -1,4 +1,4 @@
 #Spotify credentials
 CLIENT_ID = ""
 CLIENT_SECRET = ""
-REDIRECT_URI = ""
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
--- spotify/util.py
+++ spotify/util.py
@@ -2,8 +2,10 @@ from .models import SpotifyToken
 from django.utils import timezone
 from datetime import timedelta
 from .credentials import CLIENT_ID, CLIENT_SECRET
-from requests import post
+from requests import post, put, get
 
+
+BASE_URL = "https://api.spotify.com/v1/me/"
 
 
 def get_user_tokens(session_id):
